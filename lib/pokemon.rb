dffe5ab3f774c5db18db54acab8fb841e0a022bd
Leah Hirschfeld <leah.hirschfeld@gmail.com>
2020-11-14 22:45:50 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -21,6 +21,6 @@ class Pokemon
   def self.find(id)
     sql = "SELECT * FROM pokemon WHERE id = ?"
     result = DB[:conn].execute(sql, id)[0]
-    Song.new(result[0], result[1], result[2])
+    Pokemon.new(result[0], result[1], result[2])
   end
 end
